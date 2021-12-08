84e703a7cbc4af4ee651d44087b5629e11496fa9
Ryan Prescott <rppgoat@gmail.com>
2021-12-07 21:37:20 -0500

    Fix nav in retro theme and start adding content to home page

--- sites/default/pages/home.php
+++ sites/default/pages/home.php
@@ -10,3 +10,10 @@ description=Rugosa is an object-oriented web framework.
 	<p>To get started, replace the content of this page with anything of your choosing!</p>
 	<hr>
 </div>
+
+<h2>Rugosa - what is it?</h2>
+<p>
+	There are many, many better options for creating a website. I created
+	this framework to satisfy a deep-seated need to constantly create. It
+	serves no real purpose. If someone wants to use it, that's great!
+</p>
--- sites/default/themes/retro/parts/nav.php
+++ sites/default/themes/retro/parts/nav.php
@@ -4,12 +4,9 @@
 <div class="simple-nav">
 	<ul>
 		<a href="/"><li><strong><?=$r->site->title?></strong></li></a>
-		<a href="/"><li>Home</li></a>
-		<a href="/"><li>About</li></a>
-		<a href="/"><li>Contact</li></a>
-		<a href="/"><li>Explore</li></a>
-		<a href="/"><li>Compare</li></a>
-		<a href="/"><li>Actualize</li></a>
+		<?php 
+			$r->nav->build($r->pages); 
+		?>
 		
 	</ul>
 </div>
