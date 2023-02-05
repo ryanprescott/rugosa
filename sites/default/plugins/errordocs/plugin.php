66355a9b95a3aa9bea240aac552100567e21c7d8
Ryan Prescott <rppgoat@gmail.com>
2023-02-05 16:00:20 -0500

    Update errordocs with new syntax

--- sites/default/plugins/errordocs/plugin.php
+++ sites/default/plugins/errordocs/plugin.php
@@ -1,17 +1,17 @@
-<?php namespace Rugosa;
+<?php namespace RugosaErrorDocs;
 /*--
-name=errordocs
-friendlyname=Error Documents
+name=rugosa_errordocs
+friendlyname=Rugosa Error Documents
 description=Simple error documents without having to create any pages.
 --*/
 
-$r->hook("after_load_pages", function() use ($r) {
-    $content_func = function() use ($r) {
-        http_response_code($r->page->name);
+\Rugosa\hook("after_load_pages", function() {
+    $content_func = function() {
+        http_response_code(\Rugosa\page->name);
         ?>
         <div class="hero">
-        <h1><?=$r->page->title?></h1>
-        <p><?=$r->page->description?></p>
+        <h1><?=\Rugosa\page->title?></h1>
+        <p><?=\Rugosa\page->description?></p>
         <p><a href="/">Go Home</a></p>
         <hr>
         </div>
@@ -22,9 +22,9 @@ $r->hook("after_load_pages", function() use ($r) {
         ["500","500 Server Error","The server has encountered an error. Please try again later."]
     ];    
     foreach ($skeletons as $skeleton) {
-        if (!isset($r->pages->items[$skeleton[0]])) {
+        if (!isset(\Rugosa\pages->items[$skeleton[0]])) {
             $arr = ["name"=>$skeleton[0],"title"=>$skeleton[1],"description"=>$skeleton[2],"content"=>$content_func,"hidden"=>true];
-            $r->pages->add(new \Rugosa\Page($arr));
+            \Rugosa\pages->add(new \Rugosa\Page($arr));
         }
     }
 });
